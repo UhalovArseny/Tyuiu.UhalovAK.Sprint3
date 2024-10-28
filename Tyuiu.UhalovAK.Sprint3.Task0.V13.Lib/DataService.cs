62b61f1bfb75e1a0e37ae9c560e04cb2248a3b70
Uhalov Arseny <akukhalov@gmail.com>
2024-10-29 01:34:08 +0500

    Task0

--- Tyuiu.UhalovAK.Sprint3.Task0.V13.Lib/DataService.cs
+++ Tyuiu.UhalovAK.Sprint3.Task0.V13.Lib/DataService.cs
@@ -8,9 +8,9 @@ namespace Tyuiu.UhalovAK.Sprint3.Task0.V13.Lib
         {
             double S = 0;
             int i;
-            for (i = startValue; i <= stopValue; i++) ;
+            for (i = startValue; i <= stopValue; i++);
             {
-                S = S +(( Math.Pow(0.5, i) + (1/2))+ Math.Cos(i));
+                S = S +(( Math.Pow(value, i) + 0.5)* Math.Cos(i));
             }
             return Math.Round(S, 3);
         }
