6584b3920cdd7e9252a84cf3c408bc0f2cba1d24
Uhalov Arseny <akukhalov@gmail.com>
2024-11-01 02:47:35 +0500

    Task4

---
+++ Tyuiu.UhalovAK.Sprint3.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.UhalovAK.Sprint3.Task4.V2.Lib;
+namespace Tyuiu.UhalovAK.Sprint3.Task4.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculateValid()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 0.104;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.UhalovAK.Sprint3.Task4.V2/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.UhalovAK.Sprint3.Task4.V2.Lib;
+namespace Tyuiu.UhalovAK.Sprint3.Task4.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Оператор switch                                                    *");
+            Console.WriteLine("* Задание #4                                                               *");
+            Console.WriteLine("* Вариант #2                                                             *");
+            Console.WriteLine("* Выполнил: Ухалов Арсений Константинович | АСОиУБ-24-1                    *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            DataService ds = new DataService();
+
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Результат = " + ds.Calculate(startValue, stopValue)); 
+            Console.ReadKey();
+        }
+    }
+}
